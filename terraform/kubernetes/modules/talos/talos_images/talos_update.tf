90ce82fa85d894a1e5c68bb682a9546ed26a02d8
cedsbe <8707686+cedsbe@users.noreply.github.com>
2025-11-11 11:51:08 +0100

    Improve talos_image

--- terraform/kubernetes/modules/talos/talos_images/talos_update.tf
+++ terraform/kubernetes/modules/talos/talos_images/talos_update.tf
@@ -4,16 +4,20 @@
 #
 ###############################################################
 
-# data "talos_image_factory_versions" "update" {
-#   filters = {
-#     version = var.talos_image_update.version
-#   }
-# }
+data "talos_image_factory_versions" "update" {
+  filters = {
+    version = var.talos_image_update.version
+  }
+}
 
 locals {
   # Validate that the requested version is available
+  talos_image_update_version = contains(data.talos_image_factory_versions.update.talos_versions, var.talos_image_update.version) ? var.talos_image_update.version : null
+
+  # Build effective configuration by merging update config with base defaults
+  # Any unspecified update parameter falls back to the base configuration
   talos_image_update_built = {
-    version         = coalesce(var.talos_image_update.version, var.talos_image_base.version)
+    version         = coalesce(local.talos_image_update_version, var.talos_image_base.version)
     extensions      = coalesce(var.talos_image_update.extensions, var.talos_image_base.extensions)
     extraKernelArgs = coalesce(var.talos_image_update.extraKernelArgs, var.talos_image_base.extraKernelArgs)
     architecture    = coalesce(var.talos_image_update.architecture, var.talos_image_base.architecture)
@@ -45,16 +49,4 @@ data "talos_image_factory_urls" "update" {
   schematic_id  = talos_image_factory_schematic.update.id
   architecture  = local.talos_image_update_built.architecture
   platform      = local.talos_image_update_built.platform
-}
-
-output "image_update" {
-  description = "Update Talos image configuration"
-  value = {
-    architecture = local.talos_image_update_built.architecture
-    image_id     = "${talos_image_factory_schematic.update.id}_${local.talos_image_update_built.version}"
-    platform     = var.talos_image_update.platform
-    schematic_id = talos_image_factory_schematic.update.id
-    url          = data.talos_image_factory_urls.update.urls.disk_image
-    version      = local.talos_image_update_built.version
-  }
 }
